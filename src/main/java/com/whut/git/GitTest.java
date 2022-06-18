0a4749fd8a025059173fc8661653adf51232fcd6
Ypj <Ypj@qq.com>
2022-06-18 16:44:05 +0800

    push2 commit

--- src/main/java/com/whut/git/GitTest.java
+++ src/main/java/com/whut/git/GitTest.java
@@ -11,7 +11,7 @@ public class GitTest {
         System.out.println("hello git3!");
         System.out.println("hello git4!");
         System.out.println("master test!");
-        System.out.println("github push test!");
+        System.out.println("github push2 test!");
         System.out.println("github pull test!");
     }
 }
