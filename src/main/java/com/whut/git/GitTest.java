ab5c76ce2c223cba1a2664706e99b7c94353128f
Ypj <Ypj@qq.com>
2022-06-18 14:32:27 +0800

    first commit

---
+++ src/main/java/com/whut/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.whut.git;
+
+/**
+ * @author yuan
+ * @date 2022/6/18 - 14:25
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
